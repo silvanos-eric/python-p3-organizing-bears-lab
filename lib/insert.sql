f2d88dd88412dc707e19fa06146e3f7a5f7cca94
Silvanos Eric <ericoderito@gmail.com>
2024-09-12 16:28:32 +0300

    feat(database): add bear table and queries
    - Create `bears` table with columns for id, name, age, sex, color, temperament, and alive status.
    - Insert sample data into `bears` table.
    - Update SQL queries for selecting and ordering bear data:
      - Retrieve names and ages of all female bears.
      - Retrieve names of all bears in alphabetical order.
      - Retrieve names and ages of all alive bears, ordered from youngest to oldest.
      - Retrieve the oldest bear's name and age.
      - Retrieve the youngest bear's name and age.

---
+++ lib/insert.sql
@@ -0,0 +1,60 @@
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  (
+    'Mr. Chocolate',
+    5,
+    'M',
+    'Black',
+    'Easygoing',
+    true
+  );
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Rowdy', 12, 'M', 'Brown', 'Sociable', true);
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Tabitha', 7, 'F', 'White', 'Energetic', false);
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  (
+    'Sergeant Brown',
+    15,
+    'M',
+    'Cinnamon',
+    'Sensitive',
+    false
+  );
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  (
+    'Melissa',
+    8,
+    'F',
+    'Blonde',
+    'Introverted',
+    false
+  );
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Grinch', 10, 'M', 'Gray', 'Extroverted', true);
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Wendy', 13, 'F', 'Dark brown', 'Moody', false);
+
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  (NULL, 4, 'M', 'Tawny', 'Cautious', true);
